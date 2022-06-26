ca7cc19752f8ef68a682f5d64b67aa7cc1717094
Oh Ga eun <dhrkdms4113@naver.com>
2022-06-26 22:26:01 +0900

    create 13:30

--- src/App.js
+++ src/App.js
@@ -26,6 +26,21 @@ function Nav(props){
   </ol>
 </nav>
 }
+function Create(props){
+  return <article>
+    <h2>Create</h2>
+    <form onSubmit={event=>{
+      event.preventDefault();
+      const title = event.target.title.value;
+      const body = event.target.body.value;
+      props.onCreate(title, body);
+    }}>
+      <p><input type="text" name="title" placeholder="title"/></p>
+      <p><textarea name="body" placeholder="body"></textarea></p>
+      <p><input type="submit" value="Create"/></p>
+    </form>
+  </article>
+}
 function Acticle(props){
   return <acticle>
   <h2>{props.title}</h2>
@@ -38,11 +53,12 @@ function App() {
   // const setMode = _mode[1];
   const [mode, setMode] = useState('WELCOME');
   const [id, setId] =useState(null);
-  const topics = [
+  const [nextId, setNextId] = useState(4);
+  const [topics, setTopics] = useState([
     {id:1, title:'html', body:'html is...'},
     {id:2, title:'css', body:'css is...'},
     {id:3, title:'javascript', body:'javascript is...'}
-  ]
+  ]);
   let content = null;
   if (mode === 'WELCOME'){
     content = <Acticle title="Welcome" body="Hello, WEB"></Acticle>
@@ -56,6 +72,11 @@ function App() {
       }
     }
     content = <Acticle title={title} body={body}></Acticle>
+  } else if(mode === 'CREATE') {
+    content = <Create onCreate={(_title, _body)=>{
+      const newTopic = {id:nextId, title:_title, body:_body}
+      
+    }}></Create>
   }
   return (
     <div>
@@ -67,6 +88,10 @@ function App() {
         setId(_id);
       }}></Nav>
       {content}
+      <a href='/crete' onClick={event=>{
+        event.preventDefault();
+        setMode('CREATE');
+      }}>Create</a>
     </div>
   );
 }
